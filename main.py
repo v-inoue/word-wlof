4979abe659e45b08769e7142971b35b2300bc1ce
v-inoue <tomohiro1020.i@gmail.com>
2025-08-01 01:58:39 +0900

    feat:出題レベルの追加

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import google.generativeai as genai
+from google import genai
 import dotenv
 import os
 import json
@@ -8,21 +8,26 @@ from fastapi.middleware.cors import CORSMiddleware
 from collections import deque
 from pprint import pprint
 import random
-from google.generativeai import types
+from google.genai import types
 
 # .env から APIキーを読み込み
 dotenv.load_dotenv()
-genai.configure(api_key=os.getenv("API_KEY"))
 
 # モデルを指定
-model = genai.GenerativeModel(
-    'gemini-2.5-flash',
+# model = genai.GenerativeModel(
+#     'gemini-2.5-flash',
     
-    generation_config={"response_mime_type": "application/json"}
+#     generation_config={"response_mime_type": "application/json"}
+# )
+client = genai.Client()
+# generation_config = {
+#   "temperature": 2.0, 
+# }
+generate_config = types.GenerationConfig(
+    thinking_config=types.ThinkingConfig(
+        thinking_budget=0
+    )
 )
-generation_config = {
-  "temperature": 2.0, 
-}
 
 app = fastapi.FastAPI()
 
@@ -40,6 +45,9 @@ app.add_middleware(
 
 # プロンプト作成関数
 def create_prompt():
+    # 難易度の選択肢
+    difficulty_levels =3 #random.randint(1, 5)
+    print(f"選択された難易度: {difficulty_levels}")
     if generated_word_history:
         exclusion_words = ", ".join(generated_word_history)
         exclusion_prompt = f"次の単語は使わないでください：{exclusion_words}。"
@@ -48,8 +56,12 @@ def create_prompt():
 
     prompt = (
         "# 指示\n"
-        "情報工学分野から「web」や「ソフトウェア工学」、「AI」、「プログラミング言語」などの分野をランダムに選択し、用語を四つ生成してください。"
+        "情報工学分野から「web」や「ソフトウェア工学」、「AI」、「プログラミング言語」などの分野をランダムに選択し、用語を8つ生成してください。"
+        "さらに、選択した分野のサブ分野もランダムに選んでください。"
         "また、英単語の場合は（）で日本語もつけてください。"
+        "単語の難易度を五段階のうち、"
+        f"{difficulty_levels}"
+        "の単語を生成してください。"
         "# 出力用語の例\n"
         "['useState', 'useEffect', 'button', 'localstorage'],"
         "['ウォータフォール', 'アジャイル', 'スパイラル', 'XP'],"
@@ -58,8 +70,12 @@ def create_prompt():
         "\n以下形式のみで出力してください：\n"
         "{\n"
         "  \"domain\": \"分野\",\n"
-        "  \"words\": [\"単語1\", \"単語2\", \"単語3\", \"単語4\"],\n"
-        "  \"explanations\": [\"説明1\", \"説明2\", \"説明3\", \"説明4\"]\n"
+        "  \"sub_domain\": \"分野\",\n"
+        "  \"difficulty_levels\": 1から5の数値,\n"
+        # "  \"words\": [\"単語1\", \"単語2\", \"単語3\", \"単語4\"],\n"
+        " \"  \"words\": [\"単語1\", \"単語2\", \"単語3\", \"単語4\", \"単語5\", \"単語6\", \"単語7\", \"単語8\"],\n"
+        # "  \"explanations\": [\"説明1\", \"説明2\", \"説明3\", \"説明4\"]\n"
+        "  \"explanations\": [\"単語1の説明\", \"単語2の説明\", \"単語3の説明\", \"単語4の説明\", \"単語5の説明\", \"単語6の説明\", \"単語7の説明\", \"単語8の説明\"]\n"
         "}"
     )
     return prompt
@@ -74,7 +90,17 @@ def generate_word_pair():
         "werewlof-explanation": []
     }
     prompt = create_prompt()
-    response = model.generate_content(prompt,generation_config=generation_config)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt,
+        config={
+            "response_mime_type": "application/json",
+            "thinking_config": types.ThinkingConfig(
+                thinking_budget=500
+            ),
+            "temperature": 2.0,
+        }
+    )
     output_text = response.text
 
     try:
