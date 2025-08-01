41bc22a27721f298d08ddd5bd1e7d6cc506da979
v-inoue <tomohiro1020.i@gmail.com>
2025-08-01 17:05:19 +0900

    feat:レベルの範囲を受け取りその範囲内のお題を出すように変更

--- main.py
+++ main.py
@@ -9,25 +9,17 @@ from collections import deque
 from pprint import pprint
 import random
 from google.genai import types
+from pydantic import BaseModel
 
 # .env から APIキーを読み込み
 dotenv.load_dotenv()
 
-# モデルを指定
-# model = genai.GenerativeModel(
-#     'gemini-2.5-flash',
-    
-#     generation_config={"response_mime_type": "application/json"}
-# )
 client = genai.Client()
-# generation_config = {
-#   "temperature": 2.0, 
-# }
-generate_config = types.GenerationConfig(
-    thinking_config=types.ThinkingConfig(
-        thinking_budget=0
-    )
-)
+
+class RequestData(BaseModel):
+    minLevel: int
+    maxLevel: int
+
 
 app = fastapi.FastAPI()
 
@@ -44,9 +36,9 @@ app.add_middleware(
 )
 
 # プロンプト作成関数
-def create_prompt():
+def create_prompt(difficulty_levels):
     # 難易度の選択肢
-    difficulty_levels =3 #random.randint(1, 5)
+   
     print(f"選択された難易度: {difficulty_levels}")
     if generated_word_history:
         exclusion_words = ", ".join(generated_word_history)
@@ -56,7 +48,7 @@ def create_prompt():
 
     prompt = (
         "# 指示\n"
-        "情報工学分野から「web」や「ソフトウェア工学」、「AI」、「プログラミング言語」などの分野をランダムに選択し、用語を8つ生成してください。"
+        "情報工学分野から「web」や「ソフトウェア工学」、「AI」、「プログラミング言語」などの分野をランダムに選択し、用語を4つ生成してください。"
         "さらに、選択した分野のサブ分野もランダムに選んでください。"
         "また、英単語の場合は（）で日本語もつけてください。"
         "単語の難易度を五段階のうち、"
@@ -71,25 +63,26 @@ def create_prompt():
         "{\n"
         "  \"domain\": \"分野\",\n"
         "  \"sub_domain\": \"分野\",\n"
-        "  \"difficulty_levels\": 1から5の数値,\n"
-        # "  \"words\": [\"単語1\", \"単語2\", \"単語3\", \"単語4\"],\n"
-        " \"  \"words\": [\"単語1\", \"単語2\", \"単語3\", \"単語4\", \"単語5\", \"単語6\", \"単語7\", \"単語8\"],\n"
-        # "  \"explanations\": [\"説明1\", \"説明2\", \"説明3\", \"説明4\"]\n"
-        "  \"explanations\": [\"単語1の説明\", \"単語2の説明\", \"単語3の説明\", \"単語4の説明\", \"単語5の説明\", \"単語6の説明\", \"単語7の説明\", \"単語8の説明\"]\n"
+        "  \"difficulty_level\": \"難易度\",\n"
+        "  \"words\": [\"単語1\", \"単語2\", \"単語3\", \"単語4\"],\n"
+        "  \"explanations\": [\"説明1\", \"説明2\", \"説明3\", \"説明4\"]\n"
         "}"
     )
     return prompt
 
 
 # 単語ペア生成関数
-def generate_word_pair():
+def generate_word_pair(min_level, max_level):
     send_data = {
         "citizen": [],
         "werewlof": [],
         "citizen-explanation": [],
-        "werewlof-explanation": []
+        "werewlof-explanation": [],
+        "domain": "",
+        "level": int
     }
-    prompt = create_prompt()
+    difficulty_levels = random.randint(min_level, max_level)
+    prompt = create_prompt(difficulty_levels)
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=prompt,
@@ -123,14 +116,22 @@ def generate_word_pair():
     send_data["werewlof"] = data["words"][index2]
     send_data["citizen-explanation"] = data["explanations"][index1]
     send_data["werewlof-explanation"] = data["explanations"][index2]
+    send_data["domain"] = data["domain"]
+    send_data["level"] = difficulty_levels
 
 
     return send_data
 
 # API エンドポイント
-@app.get("/generate-word-pair")
-async def get_word_pair():
-    result = generate_word_pair()
+
+@app.post("/generate-word-pair")
+
+async def get_word_pair(request: RequestData):
+
+    # リクエストデータから難易度を取得
+    min_level = request.minLevel
+    max_level = request.maxLevel
+    result = generate_word_pair(min_level, max_level)
     if "error" not in result:
         return result
     else:
